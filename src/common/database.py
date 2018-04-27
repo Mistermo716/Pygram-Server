d03cd15779a978c650f0f3a59355ff529aabbb7f
Muaath Alaraj <Muaath.Alaraj@gmail.com>
2018-04-27 10:42:44 -0700

    again

--- src/common/database.py
+++ src/common/database.py
@@ -12,7 +12,7 @@ class Database(object):
     def initialize():
         # static URI accessed through class
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client['heroku_bzlbb14n']
+        Database.DATABASE = client.get_database('heroku_bzlbb14n')
 
     @staticmethod
     def insert(collection, data):
